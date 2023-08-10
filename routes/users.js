fc88723ec24fe5baa339f666865227c955093c45
oladsdan <104092480+oladsdan@users.noreply.github.com>
2023-08-10 08:28:42 +0100

    how to authenticate user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUserId, updateUser } from "../controllers/userControllers";
+import { verifyToken } from "../utils/verifyToken";
 
 
 const router = express.Router();
+
+//using the jwt in utils folder to verify users
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("hello user, your are logged in")
+})
 
 //Update
 router.put("/:id", updateUser);
---
+++ utils/verifyToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { createError } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if(!token){
+        return next(createError(401, "You are not authenticated"))
+    }
+
+    // then we verify the token if true
+    jwt.verify(token, process.env.jwt_Access_Token, (err, user) => {
+        if(err) return next(createError(403, "Token is not valid!"))
+        req.user = user;
+        next()
+    });
+}
